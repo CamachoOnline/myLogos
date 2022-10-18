751721a62969bff94b9e8a3d4432343dae7f2c07
David Camacho <camachoonline@gmail.com>
2022-10-18 18:03:03 -0500

    Add files via upload

--- mylogo_BAK_20221018.php
+++ mylogo_BAK_20221018.php
@@ -30,16 +30,11 @@
             <!-- initially built by PHP limit only 12 (see css build at bottom) -->
             <?php
                 include 'scripts/db_config.php';
-                $sqllogos1 = "SELECT * FROM whologos_tbl WHERE userid = '".$_SESSION['user_id']."'"; // query
-                $response1 = mysqli_query($db, $sqllogos1); // execute query
+                $sql1 = "SELECT * FROM logos_tbl ORDER BY `id`"; // query
+                $res1 = mysqli_query($db, $sql1); // execute query
                 $inc1 = 1;
-				$logoArray = [];
-				while($logos1 = mysqli_fetch_assoc($response1)){
-					array_push($logoArray,$logos1['logoid']);
-				}
 		
 				echo '<script>';
-				echo 'const logoArray = ['.implode(",", $logoArray).'];';
 				echo 'const lgArray = [[1,2,6],[4,7,9],[2,8,11]];';
 				echo 'const foArray = [[1,2,6],[4,7,9],[14,8,11]];';
 				echo 'const fiArray = [[13,14,18],[16,19,21],[26,20,23]];';
@@ -47,25 +42,6 @@
 		
 				echo '<form class="js--form js--form--logos">';
 				echo '<section class="mlgo--section js--section mlgo--col mlgo--col-4 mlgo--gallery js--gallery">';
-			
-				if($logoArray)
-				{
-					$inc1 = 1;
-					for($i = 0; $i < count($logoArray); $i++)
-					{
-						if($inc1<13){
-							echo '<div class="mlgo--item js--item">';
-							echo '<input type="hidden" name="logofile-'.$inc1.' value="'.$logoArray[$i].'"/>';
-							echo '<button class="mlgo--logo js--logo logo-'.$inc1.' js--logo-'.$inc1.' an--fadein">';
-							echo '<i></i>';
-							echo '<div class="mlgo--remove">-</div><div class="mlgo--add">+</div>';
-							echo '</button>';
-							echo '</div>';
-						}
-						$inc1++;
-					}
-				}
-				/*
                 while($logo1 = mysqli_fetch_assoc($res1)){ // loop
 					if($inc1<13){
 						echo '<div class="mlgo--item js--item">';
@@ -78,28 +54,11 @@
 						$inc1++;
 					}
                 }
-				*/
 				echo '</section>';
 				echo '</form>';
 		
                 $inc2 = 1;
 				echo '<style>';
-			
-				if($logoArray)
-				{
-					for($i = 0; $i < count($logoArray); $i++)
-					{
-						$sql2 = "SELECT * FROM logos_tbl WHERE id=".$logoArray[$i]; // query
-						$res2 = mysqli_query($db, $sql2); // execute query
-						while($logo2 = mysqli_fetch_assoc($res2)){ // loop
-							echo '.logo-'.$inc2.' > i::before{';
-							echo 'content:url(\'images/logos/'.$logo2['file'].'\');';
-							echo '}';
-						}
-						$inc2++;
-					}
-				}
-				/*
 				$sql2 = "SELECT * FROM logos_tbl ORDER BY `id`"; // query
                 $res2 = mysqli_query($db, $sql2); // execute query
                 while($logo2 = mysqli_fetch_assoc($res2)){ // loop
@@ -108,7 +67,6 @@
 					echo '}';
                     $inc2++;
                 }
-				*/
 				echo '</style>';
                 mysqli_close($db); // close connection
             ?>	
--- mylogos_db.sql
+++ mylogos_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2022 at 12:14 AM
+-- Generation Time: Oct 19, 2022 at 01:01 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -38,35 +38,34 @@ CREATE TABLE `logos_tbl` (
 
 INSERT INTO `logos_tbl` (`id`, `file`) VALUES
 (1, '2A.png'),
-(2, '44850_InternetMarketingPros.png'),
-(3, 'AttackIQ11.png'),
-(4, 'APS_logo_2011.png'),
-(5, 'CyberoamLogo.png'),
-(6, '1785244723.jpg'),
-(7, 'Acceleprise Logo.png'),
-(8, 'akacrm.png'),
-(10, 'ClientTickler-NoSheen.png'),
-(11, 'AppsGeyserLogoTransparent.png'),
-(12, 'Kinvey-Logo-Orange-2700px.png'),
-(13, 'Linkdex-Logo.png'),
-(16, 'kuukie-logo-en.png'),
-(17, 'Uqido_logomodv2-21.png'),
-(18, 'unity.png'),
-(19, 'Solid_logo_png.png'),
-(20, 'xtinct3d logo.png'),
-(21, 'spike_it_logo.png'),
-(22, 'OpscodeLogo_Tag_FINAL.png'),
-(23, 'final.png'),
-(24, 'florida.png'),
-(25, 'TechxCentral-redone3.png'),
-(26, 'logo_network3.png'),
-(27, 'akacrm.png'),
-(28, '3d-NEW.png'),
-(29, 'SPEED_SkillStorm-Recruiter-Success-Training-white-BG.png'),
-(30, 'social_fuse_Logo_MMME.png'),
-(31, 'Sherry Logo Options-01finalcropped.png'),
-(32, 'AntiDrug-Alliance.png'),
-(33, 'CreativeWallGraphics_LOGO_Final.png');
+(2, '3d-NEW.png'),
+(3, '44850_InternetMarketingPros.png'),
+(4, '1785244723.jpg'),
+(5, 'Acceleprise Logo.png'),
+(6, 'akacrm.png'),
+(7, 'AntiDrug-Alliance.png'),
+(8, 'AppsGeyserLogoTransparent.png'),
+(9, 'APS_logo_2011.png'),
+(10, 'AttackIQ11.png'),
+(11, 'ClientTickler-NoSheen.png'),
+(12, 'CreativeWallGraphics_LOGO_Final.png'),
+(13, 'CyberoamLogo.png'),
+(14, 'final.png'),
+(15, 'florida.png'),
+(16, 'Kinvey-Logo-Orange-2700px.png'),
+(17, 'kuukie-logo-en.png'),
+(18, 'Linkdex-Logo.png'),
+(19, 'logo_network3.png'),
+(20, 'OpscodeLogo_Tag_FINAL.png'),
+(21, 'Sherry Logo Options-01finalcropped.png'),
+(22, 'social_fuse_Logo_MMME.png'),
+(23, 'Solid_logo_png.png'),
+(24, 'SPEED_SkillStorm-Recruiter-Success-Training-white-BG.png'),
+(25, 'spike_it_logo.png'),
+(26, 'TechxCentral-redone3.png'),
+(27, 'unity.png'),
+(28, 'Uqido_logomodv2-21.png'),
+(29, 'xtinct3d-logo.png');
 
 -- --------------------------------------------------------
 
@@ -90,6 +89,50 @@ CREATE TABLE `users_tbl` (
 INSERT INTO `users_tbl` (`id`, `fullname`, `username`, `password`, `email`, `active`) VALUES
 (1, 'David T Camacho', 'DavidTCamacho', 'TheHulk$2022', 'camachoonline@gmail.com', 1);
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `whologos_tbl`
+--
+
+CREATE TABLE `whologos_tbl` (
+  `id` int(11) NOT NULL COMMENT 'Id',
+  `userid` int(11) NOT NULL COMMENT 'UserId',
+  `logoid` int(11) NOT NULL COMMENT 'LogoId'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `whologos_tbl`
+--
+
+INSERT INTO `whologos_tbl` (`id`, `userid`, `logoid`) VALUES
+(1, 1, 1),
+(2, 1, 2),
+(4, 1, 3),
+(5, 1, 4),
+(7, 1, 5),
+(8, 1, 6),
+(9, 1, 7),
+(10, 1, 8),
+(11, 1, 9),
+(12, 1, 10),
+(13, 1, 11),
+(14, 1, 12),
+(15, 1, 13),
+(16, 1, 14),
+(17, 1, 15),
+(18, 1, 16),
+(19, 1, 17),
+(20, 1, 18),
+(21, 1, 19),
+(22, 1, 20),
+(23, 1, 21),
+(24, 1, 22),
+(25, 1, 23),
+(26, 1, 24),
+(27, 1, 25),
+(28, 1, 26);
+
 --
 -- Indexes for dumped tables
 --
@@ -107,6 +150,13 @@ ALTER TABLE `users_tbl`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `whologos_tbl`
+--
+ALTER TABLE `whologos_tbl`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `userid` (`userid`,`logoid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -114,13 +164,19 @@ ALTER TABLE `users_tbl`
 -- AUTO_INCREMENT for table `logos_tbl`
 --
 ALTER TABLE `logos_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id', AUTO_INCREMENT=34;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id', AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `users_tbl`
 --
 ALTER TABLE `users_tbl`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id', AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `whologos_tbl`
+--
+ALTER TABLE `whologos_tbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Id', AUTO_INCREMENT=51;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
